28d4f74bb5f7fbd5c446989bf18e9d329e702b3a
Brandon <green.finite@gmail.com>
2015-10-03 13:54:47 -0700

    Profile edit button minus functionality

--- HomePage.js
+++ HomePage.js
@@ -2,3 +2,12 @@ function showDiv() {
   document.getElementById('loginBoxEmail').style.display = "block";
   document.getElementById('loginBoxPass').style.display = "block";
 }
+
+$(document).ready(function(){
+  $( "#signInButton" ).click(function() {
+    document.getElementById('loginBoxEmail').style.display = "block";
+    document.getElementById('loginBoxPass').style.display = "block";
+  });
+});
+
+#$(selector).action()

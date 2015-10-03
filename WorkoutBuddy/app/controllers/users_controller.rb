5238c166648075bbad0b26cc7743d51632e9cf98
Brandon <green.finite@gmail.com>
2015-10-03 16:36:38 -0700

    Edit form appears but not where wanted

--- WorkoutBuddy/app/controllers/users_controller.rb
+++ WorkoutBuddy/app/controllers/users_controller.rb
@@ -17,6 +17,14 @@ class UsersController < ApplicationController
         @user = User.last
     end
 
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
     private
     def user_params
         params.require(:user)
